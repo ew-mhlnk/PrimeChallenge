0d40f6811175bc11f1e8b1fdfec3f0d49434ec27
Jenkaururu <jenkaururu@gmail.com>
2025-04-12 00:00:53 +0300

    Updated backend and frontend for Telegram auth and new users table

--- backend/database/db.py
+++ backend/database/db.py
@@ -1,11 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from .models import Base
+import os
 
-DATABASE_URL = "postgresql://user:password@localhost:5432/prime_bracket"  # Замени на свой
+DATABASE_URL = os.getenv("DATABASE_URL")  # Бери из переменной окружения
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# Создание таблицы
 Base.metadata.drop_all(bind=engine)  # Удаляет старую таблицу (опционально)
-Base.metadata.create_all(bind=engine)  # Создаёт новую
+Base.metadata.create_all(bind=engine)
