c52254cc0b2d1eeee03275c273fd864b50b6d5fe
Jenkaururu <jenkaururu@gmail.com>
2025-04-22 18:58:51 +0300

    не работал деплой поменяли что-то в дбпу

--- backend/database/db.py
+++ backend/database/db.py
@@ -1,24 +1,18 @@
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+from database.models import Base  # Импортируем Base из models
 
-# Используем переменную окружения DATABASE_URL, предоставляемую Render
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:password@localhost:5432/dbname")
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
-# Создаём движок SQLAlchemy
-engine = create_engine(DATABASE_URL)
-
-# Создаём фабрику сессий
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# Создаём базовый класс для моделей SQLAlchemy
 Base = declarative_base()
 
-# Функция для получения сессии базы данных
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+def init_db():
+    """
+    Создаёт все таблицы в базе данных, определённые в models.py.
+    """
+    Base.metadata.create_all(bind=engine)
