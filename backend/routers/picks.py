4851d92a8ad1a4af753fd5ba2e59f5a65706a11e
Jenkaururu <jenkaururu@gmail.com>
2025-04-29 22:39:25 +0300

    аус пу деплой

--- backend/routers/picks.py
+++ backend/routers/picks.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from database.db import get_db
 from database import models
-from utils.auth import get_current_user
+from auth import get_current_user
 import logging
 
 router = APIRouter()
